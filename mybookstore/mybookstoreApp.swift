e5f03c36676658c38d444d33bae8ade14436765b
JiJunmo <2462030021@qq.com>
2026-06-09 17:15:28 +0800

    Initial Commit

---
+++ mybookstore/mybookstoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  mybookstoreApp.swift
+//  mybookstore
+//
+//  Created by 鸡小葵 on 9/6/26.
+//
+
+import SwiftUI
+
+@main
+struct mybookstoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
